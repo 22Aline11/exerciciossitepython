bf1ac9df4fc0ab6b1674df90fb4bbb600324821d
Aline Souza <aln.aline@hotmail.com>
2023-08-02 17:59:06 -0300

    Exercicios até o 5

---
+++ EstruturaSequencial.py
@@ -0,0 +1,29 @@
+# 1. Faça um Programa que mostre a mensagem "Alo mundo" na tela. 
+print("Alo mundo!")
+
+#2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+numero = int(input("Escreva um número: "))
+print("O número digitado foi ", numero)
+
+#3. Faça um Programa que peça dois números e imprima a soma. 
+num_1 = int(input("Coloque o primeiro número: "))
+num_2 = int(input("Coloque o segundo número: "))
+soma = num_1 + num_2
+print("O valor total é: ", soma)
+
+#4. Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+nota_1 = int(input("Primeira nota: "))
+nota_2 = int(input("Segunda nota: "))
+nota_3 = int(input("Terceira nota: "))
+nota_4 = int(input("Quarta nota: "))
+media = int((nota_1 + nota_2 + nota_3 + nota_4) / 4)
+
+print(media)
+
+#5. Faça um Programa que converta metros para centímetros. 
+metro = int(input("Coloque o número de metros: "))
+centimetros = 100
+conversao = metro * centimetros
+print( metro, "metros são", conversao, "centimetros.")
+
+6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
---
+++ teste.py
@@ -0,0 +1,4 @@
+metro = int(input("Coloque o número de metros: "))
+centimetros = 100
+conversao = metro * centimetros
+print( metro, "metros são", conversao, "centimetros.")
