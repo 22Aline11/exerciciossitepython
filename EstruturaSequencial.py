2e67a548fe12ce8b35dcfdfb1b20598d04251550
Aline Souza <aln.aline@hotmail.com>
2023-08-02 21:34:42 -0300

    Ex. 16

--- EstruturaSequencial.py
+++ EstruturaSequencial.py
@@ -143,3 +143,18 @@ print("Salário Liquido : R$  {:.2f}".format(salario_liq))
 #16. Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. 
 # Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. 
 # Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total. 
+import math
+
+area = float(input("Coloque a área a ser pintada em metros quadrados: "))
+litros = area / 3
+lata_tinta = math.ceil(litros / 18)
+unidade = 80
+
+if litros <= 18:
+    print("Compre 1 galão de tinta")
+    print("Preço total: R$ {:.2f}".format(unidade))
+else:
+    latas_adicionais = lata_tinta - 1 if litros % 18 == 0 else lata_tinta
+    preco_total = latas_adicionais * unidade
+    print("Compre {} galões de tinta".format(latas_adicionais))
+    print("Preço total: R$ {:.2f}".format(preco_total))
